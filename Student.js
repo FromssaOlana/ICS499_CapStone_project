810fd2749ae317d140b1edb6c480302c300c66c9
FromssaOlana <zz4396bs@metrostate.edu>
2020-03-06 23:24:03 -0600

    student object file added

---
+++ Student.js
@@ -0,0 +1,27 @@
+
+// A student object 
+let Student = {
+    name: undefined,
+    studentId: undefined,
+    userName: undefined,
+    password: undefined,
+    employer: Employer,
+    applications: [],
+    login: function(userName, password){
+
+    },
+    logout: function(){
+
+    },
+    creatApplication: function(){
+
+    },
+    signApplication: function() {
+        
+    }, 
+    viewApplication: function(){
+
+    }
+    
+
+}
