6e842cca7174304cc51974c6b2fce9f8b2185d1f
FromssaOlana <zz4396bs@metrostate.edu>
2020-03-09 22:32:26 -0500

    Updated Student prototype

--- Student.js
+++ Student.js
@@ -1,27 +1,40 @@
-
 // A student object 
-let Student = {
-    name: undefined,
-    studentId: undefined,
-    userName: undefined,
-    password: undefined,
-    employer: Employer,
-    applications: [],
-    login: function(userName, password){
-
-    },
-    logout: function(){
-
-    },
-    creatApplication: function(){
-
-    },
-    signApplication: function() {
-        
-    }, 
-    viewApplication: function(){
-
-    }
-    
+function Student(name, studentId) {
+    this.name = name;
+    this.studentId = studentId;
+    this.userName = undefined;
+    this.password = undefined;
+    this.employer = undefined;
+    this.applications = undefined;
+    this.applicationsStatus= false;
 
 }
+
+// logs in the student account 
+Student.prototype.login = function(userName, password){
+    /// do Stuff
+
+};
+
+// logs out the student account
+Student.prototype.logout = function(){
+    /// do Stuff
+};
+// creats a application document
+Student.prototype.creatApplication = function(){
+    /// do Stuff
+};
+
+// sighns the document
+Student.prototype.signApplication = function(){
+    /// do Stuff
+};
+
+// views the application status
+Student.prototype.viewApplicationStatus = function(){
+    /// do Stuff
+};
+
+
+let Fromssa =  new Student('Fromssa', 0101);
+
