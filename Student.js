3ba9c99c2922465c504bdaff0cacbc637abf8bb6
FromssaOlana <zz4396bs@metrostate.edu>
2020-03-09 23:51:56 -0500

    Added an Employer prototype. some of the methods are not implemented fully

--- Student.js
+++ Student.js
@@ -5,11 +5,20 @@ function Student(name, studentId) {
     this.userName = undefined;
     this.password = undefined;
     this.employer = undefined;
-    this.applications = undefined;
-    this.applicationsStatus= false;
+    this.applications = [];
+    this.applicationsStatus= undefined;
 
 }
 
+
+// sets username 
+Student.prototype.setUserName = function(userName){
+    this.userName = userName;
+ };
+ // sets user password
+ Student.prototype.setPassWord = function(password){
+     this.password = password;
+  };
 // logs in the student account 
 Student.prototype.login = function(userName, password){
     /// do Stuff
@@ -23,18 +32,38 @@ Student.prototype.logout = function(){
 // creats a application document
 Student.prototype.creatApplication = function(){
     /// do Stuff
+    // just for a test 
+    this.applications.push(function application(){
+            applName: "Targer intern"
+    })
 };
+
+Student.prototype.findApplication = function(appName){
+    return this.applications.find
+}
 
 // sighns the document
 Student.prototype.signApplication = function(){
     /// do Stuff
 };
 
-// views the application status
-Student.prototype.viewApplicationStatus = function(){
-    /// do Stuff
+// set the application status
+Student.prototype.setApplicationStatus = function(status){
+     this.applicationsStatus = status;
 };
 
+// views the application status
+Student.prototype.viewApplicationStatus = function(){
+    return this.applicationsStatus;
+};
 
-let Fromssa =  new Student('Fromssa', 0101);
+// sets employer
+Student.prototype.setEmployer = function(employer){
+        this.employer = employer;
+};
+
+Student.prototype.addApliction = function(application){
+        this.applications = application;
+};
+
 
